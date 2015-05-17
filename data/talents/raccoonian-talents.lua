8d705f7f67b0b281fb8427cf9e59d2adbbd0be1e
Ramonacus <ramonacus@gmail.com>
2015-05-18 01:09:05 +0200

    Implementation of Raccoonian Rampage.

--- data/talents/raccoonian-talents.lua
+++ data/talents/raccoonian-talents.lua
@@ -31,61 +31,13 @@ newTalent {
     end,
     on_unlearn = function(self, t)
         self.inc_stats[self.STAT_DEX] = self.inc_stats[self.STAT_DEX] - 1
-        self:onStatChange(self.STAT_DEX, - 1)
+        self:onStatChange(self.STAT_DEX, -1)
         self.inc_stats[self.STAT_CUN] = self.inc_stats[self.STAT_CUN] - 3
-        self:onStatChange(self.STAT_CUN, - 3)
+        self:onStatChange(self.STAT_CUN, -3)
     end,
     info = function(self, t)
         return ([[Boosts your racial aptitudes for malice and thievery.
 Increases Cunning by %d and Dexterity by %d.]]):format(3 * self:getTalentLevelRaw(t), self:getTalentLevelRaw(t))
-    end,
-}
-
--- Raccoonian musk. (Copied from cursed earth for the moment)
-newTalent {
-    name = "Raccoonian Musk",
-    type = { "race/raccoonian", 1 },
-    require = racial_req1,
-    points = 5,
-    no_energy = true,
-    no_npc_use = true,
-    cooldown = 30,
-    radius = function(self, t)
-        return math.floor(self:combatScale(self:getTalentLevel(t), 4, 0, 6, 5))
-    end,
-    range = 1,
-    random_ego = "defensive",
-    tactical = { DEFEND = 2 },
-    getDuration = function(self, t)
-        return math.min(15, 8 + math.floor(self:getTalentLevel(t)))
-    end,
-    getIncDamage = function(self, t)
-        return math.floor(math.min(40, 15 + (math.sqrt(self:getTalentLevel(t)) - 1) * 23))
-    end,
-    getPowerPercent = function(self, t)
-        return math.floor((math.sqrt(self:getTalentLevel(t)) - 1) * 20)
-    end,
-    action = function(self, t)
-        local range = self:getTalentRange(t)
-        local duration = t.getDuration(self, t)
-        local incDamage = t.getIncDamage(self, t)
-
-        -- project first to immediately start the effect
-        local tg = {type="ball", radius=range}
-        self:project(tg, self.x, self.y, DamageType.WEAKNESS, { incDamage=incDamage, dur=3 })
-
-        game.level.map:addEffect(self,
-            self.x, self.y, duration,
-            DamageType.WEAKNESS, { incDamage=incDamage, dur=3 },
-            range,
-            5, nil,
-            MapEffect.new{color_br=20, color_bg=220, color_bb=70, effect_shader="shader_images/poison_effect.png"}
-        )
-
-        return true
-    end,
-    info = function(self, t)
-        return ([[Mark your surroundings with you musk glands, making others feel ill-at-ease.]])
     end,
 }
 
@@ -124,12 +76,45 @@ newTalent {
     info = function(self, t)
         local rad = self:getTalentRadius(t)
         return ([[Focus on the scents and noises around you.
-Reveal all creatures in a radius of %d. Your deep focus will also slow you down a 20%% while active.]]):format(rad)
+Reveal all creatures in a radius of %d. Your deep focus will also slow you down a 20%% while active.
+Radius will increase with your Cunning.]]):format(rad)
+    end,
+}
+
+-- Rockstar Raccoon Rampage.
+newTalent {
+    name = "Rockstar Raccoon Rampage",
+    type = { "race/raccoonian", 3 },
+    require = racial_req3,
+    points = 5,
+    no_energy = true,
+    no_npc_use = true,
+    cooldown = 30,
+    range = 1,
+    random_ego = "defensive",
+    tactical = { DEFEND = 2 },
+    getMovementSpeed = function(self, t)
+        return self:combatScale(self:getCun(10, true) * self:getTalentLevel(t), .3, 0, 1, 50)
+    end,
+    getDuration = function(self, t)
+        return (4 + math.floor(self:getTalentLevel(t) / 2))
+    end,
+    action = function(self, t)
+        local duration = t.getDuration(self, t)
+        local movementSpeed = t.getMovementSpeed(self, t)
+
+        self:setEffect(self.EFF_RACCOONIAN_RAMPAGE, duration, { power = movementSpeed })
+
+        return true
+    end,
+    info = function(self, t)
+        return ([[Enter a destructive rampage state, boosting your movement speed by %d%% for %d turns.
+Every movement action will let you unleash your destructive behaviour, granting a free attack to a random adjacent enemy as phyisical damage.]]):format(100 * t.getMovementSpeed(self, t), t.getDuration(self, t))
     end,
 }
 
 -- Raccoonian rage.
-newTalent{
+newTalent {
     name = "Raccoonian Rage",
     type = { "race/raccoonian", 4 },
     mode = "passive",
@@ -142,7 +127,7 @@ newTalent{
         return t.maxSpeedup(self, t) * (1 - (self.life / self.max_life))
     end,
     callbackOnAct = function(self, t)
-        self:setEffect(self.EFF_RACCOONIAN_RAGE, 1, {power=t.speedup(self, t)})
+        self:setEffect(self.EFF_RACCOONIAN_RAGE, 1, { power = t.speedup(self, t) })
     end,
     info = function(self, t)
         return ([[Your animal nature makes you react to pain and wounds with a wild frenzy.
--- data/timed_effects/raccoonian.lua
+++ data/timed_effects/raccoonian.lua
@@ -1,7 +1,8 @@
 --
--- Timed
+-- Timed effects for Raccoonians.
 --
 
+-- Raccoonian rage.
 newEffect{
     name = "RACCOONIAN_RAGE",
     image = "talents/raccoonian_rage.png",
@@ -25,5 +26,44 @@ newEffect{
     end,
     deactivate = function(self, eff)
         self:removeTemporaryValue("global_speed_add", eff.tmpid)
+    end
+}
+local Map = require "engine.Map"
+local DamageType = require "engine.DamageType"
+
+-- Raccoonian rampage.
+newEffect{
+    name = "RACCOONIAN_RAMPAGE",
+    image = "talents/rockstar_raccoon_rampage.png",
+    desc = "Raccoon rampage",
+    long_desc = function(self, eff)
+        return ("Increases %d%% movement speed and grants free attacks to random targets while moving."):format(eff.power * 100)
+    end,
+    type = "physical",
+    subtype = {
+        speed=true
+    },
+    status = "beneficial",
+    parameters = {
+        power = 0.2
+    },
+    callbackOnMove = function(self, eff, moved, force, ox, oy)
+        local tgts = {}
+        for _, c in pairs(util.adjacentCoords(self.x, self.y)) do
+            local target = game.level.map(c[1], c[2], Map.ACTOR)
+            if target and self:reactionToward(target) < 0 then tgts[#tgts+1] = target end
+        end
+        if #tgts > 0 then
+            self.turn_procs.raccoonian_rampage = true
+            DamageType:projectingFor(self, {project_type={talent=self:getTalentFromId(self.T_ROCKSTAR_RACCOON_RAMPAGE)}})
+            self:attackTarget(rng.table(tgts), DamageType.PHYSICAL, self:combatTalentWeaponDamage(self.T_ROCKSTAR_RACCOON_RAMPAGE, 0.5, 1.1), true)
+            DamageType:projectingFor(self, nil)
+        end
+    end,
+    activate = function(self, eff)
+        eff.tmpid = self:addTemporaryValue("movement_speed", eff.power)
+    end,
+    deactivate = function(self, eff)
+        self:removeTemporaryValue("movement_speed", eff.tmpid)
     end
 }
